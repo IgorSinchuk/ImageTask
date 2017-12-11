727222161fd59e6dc85b0013c15bef39dfe8b9f0
Igor Sinchuk <IgorSinchuk@users.noreply.github.com>
2017-12-11 20:35:34 +0200

    init

--- src/main/java/com/igorsinchuk/imagetask/Main.java
+++ src/main/java/com/igorsinchuk/imagetask/Main.java
@@ -6,51 +6,51 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class CompareTwoImages {
+public class Main {
 
     public static void main(String[] args) throws IOException {
-       CompareTwoImages cti = new CompareTwoImages("./src/main/resources/Images/image1.png",
-               "./src/main/resources/Images/image2.png");
-       cti.setParameters(10,10 );
-        cti.compare();
-        if (!cti.isIdentic()) {
+       Main compare = new Main("./src/main/resources/Images/img1.png",
+               "./src/main/resources/Images/img2.png");
+       compare.setParameters(10,10 );
+        compare.compare();
+        if (!compare.isIdentic()) {
             System.out.println("Images do not match.");
             System.out.println("See the result in the 'result' folder.");
-            CompareTwoImages.savePNG(cti.getImageResult(),
+            Main.savePNG(compare.getImageResult(),
                     "./src/main/resources/Result/result.png");
         } else {
             System.out.println("Images are the same");
         }
     }
 
-    private static BufferedImage image1, image2, imageResult;
     private static boolean isIdentic;
     private static int compareX, compareY;
+    private static BufferedImage img1, img2, imgRslt;
 
     private static double sensitivity = 0.10;
 
-    public CompareTwoImages(String file1, String file2) throws IOException {
-        image1 = loadPNG(file1);
-        image2 = loadPNG(file2);
+    public Main(String file1, String file2) throws IOException {
+        img1 = loadPNG(file1);
+        img2 = loadPNG(file2);
     }
 
     public void setParameters(int compareX, int compareY) {
-        CompareTwoImages.compareX = compareX;
-        CompareTwoImages.compareY = compareY;
+        Main.compareX = compareX;
+        Main.compareY = compareY;
     }
 
     public void compare() {
-        imageResult = new BufferedImage(image2.getWidth(null), image2.getHeight(null), BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2 = imageResult.createGraphics();
-        g2.drawImage(image2, null, null);
+        imgRslt = new BufferedImage(img2.getWidth(null), img2.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = imgRslt.createGraphics();
+        g2.drawImage(img2, null, null);
         g2.setColor(Color.RED);
-        int blocksX = (int)(image1.getWidth()/compareX);
-        int blocksY = (int)(image1.getHeight()/compareY);
-        CompareTwoImages.isIdentic = true;
+        int blocksX = (int)(img1.getWidth()/compareX);
+        int blocksY = (int)(img1.getHeight()/compareY);
+        Main.isIdentic = true;
         for (int y = 0; y < compareY; y++) {
             for (int x = 0; x < compareX; x++) {
-                int result1 [][] = convertTo2D(image1.getSubimage(x*blocksX, y*blocksY, blocksX - 1, blocksY - 1));
-                int result2 [][] = convertTo2D(image2.getSubimage(x*blocksX, y*blocksY, blocksX - 1, blocksY - 1));
+                int result1 [][] = convertTo2D(img1.getSubimage(x*blocksX, y*blocksY, blocksX - 1, blocksY - 1));
+                int result2 [][] = convertTo2D(img2.getSubimage(x*blocksX, y*blocksY, blocksX - 1, blocksY - 1));
                 for (int i = 0; i < result1.length; i++) {
                     for (int j = 0; j < result1[0].length; j++) {
                         int diff = Math.abs(result1[i][j] - result2[i][j]);
@@ -65,7 +65,7 @@ public class CompareTwoImages {
     }
 
     public BufferedImage getImageResult() {
-        return imageResult;
+        return imgRslt;
     }
 
     public int[][] convertTo2D(BufferedImage subImage) {
